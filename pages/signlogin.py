3418619977082b0fbe84d235e1aa6b8646bf16d5
RAKHUL PRAKASH S B <118678415+BlackEmpir7199@users.noreply.github.com>
2023-07-27 21:39:35 +0530

    Update signlogin.py

--- pages/signlogin.py
+++ pages/signlogin.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 
 # Initialize Deta with your Deta Project Key
-load_dotenv()
+load_dotenv(".env")
 DETA_KEY = os.getenv("DETA_KEY")
 deta = Deta(DETA_KEY)
 users_db = deta.Base("users")
