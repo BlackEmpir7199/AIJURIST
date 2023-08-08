8ede110910b8e4d377a014774f131179670bba99
RAKHUL PRAKASH S B <118678415+BlackEmpir7199@users.noreply.github.com>
2023-08-08 23:13:28 +0530

    Update courts_info.py

--- pages/courts_info.py
+++ pages/courts_info.py
@@ -90,7 +90,7 @@ def render_courts_info():
 .button {
   --border-right: 6px;
   --text-stroke-color: rgba(255,255,255,0.6);
-  --animation-color:#37c4ff;
+  --animation-color:#205fff;
   --fs-size: 2em;
   letter-spacing: 3px;
   text-decoration: none;
