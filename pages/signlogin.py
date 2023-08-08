e68798b1a094a96a8f0f9a454410d73f73a76797
RAKHUL PRAKASH S B <118678415+BlackEmpir7199@users.noreply.github.com>
2023-08-08 21:59:29 +0530

    Update signlogin.py

--- pages/signlogin.py
+++ pages/signlogin.py
@@ -230,7 +230,7 @@ def render_login_page():
     col1,col2,col3 = st.columns([0.16,0.3,0.5])
     with col2:
         st.markdown(
-            """<p style='font-size: 30px; font-weight: 700;'>
+            """<p style='font-size: 25px; font-weight: 500;'>
             <span style='color: #250fff;'>AI J</span>urist
             </p>""",
             unsafe_allow_html=True)
